9437eb82b0cd32ce8f049e57a2525c8b5b7420f6
vmurashev <vitaly.murashev@gmail.com>
2016-03-26 00:38:40 +0300

    fixes to launch build of stock python

--- build-host-python27.sh
+++ build-host-python27.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -e
 
-SYSTEMS_ALL='linux-x86,linux-x86_64,windows,windows-x86_64,darwin-x86,darwin-x86_64'
-SYSTEMS_TO_BUILD=$SYSTEMS_ALL
-#SYSTEMS_TO_BUILD='windows'
+if [ "$(uname -s)" = "Darwin" ]; then
+    SYSTEMS_ALL='darwin-x86,darwin-x86_64'
+else
+    SYSTEMS_ALL='linux-x86,linux-x86_64,windows,windows-x86_64'
+fi
 
+SYSTEMS_TO_BUILD=$SYSTEMS_ALL
+#SYSTEMS_TO_BUILD='linux-x86_64'
 
 DIR_HERE=$(cd $(dirname $0) && pwd)
 . $DIR_HERE/ndk.pth
@@ -16,6 +20,7 @@ mkdir -p $DIR_OBJ_ROOT
 PYTHON_URL='https://api.github.com/repos/crystax/android-vendor-python-2-7/tarball/master'
 PYTHON_ARC_NAME='vendor-host-python27.tgz'
 SRC_DIR="$DIR_EXTERNALS/vendor-host-python27"
+OBJ_DIR="$DIR_OBJ_ROOT/vendor-host-python27"
 
 if [ ! -d $SRC_DIR ]; then
     if [ ! -f "$DIR_EXTERNALS/$PYTHON_ARC_NAME" ]; then
@@ -25,32 +30,24 @@ if [ ! -d $SRC_DIR ]; then
     tar xvf "$DIR_EXTERNALS/$PYTHON_ARC_NAME" --strip-components=1 -C $SRC_DIR
 fi
 
-for system in $(echo $SYSTEMS_ALL | tr ',' ' '); do
-    rm -f "/var/tmp/ndk-cache-$USER/ndk-vendor-host-python2.7-$system.tar.xz"
-done
-
 # $1: system
 build_host_python ()
 {
     local SYSTEM=$1
-    local OBJ_SYS_DIR="$DIR_OBJ_ROOT/vendor-host-python27-$SYSTEM"
     local SYS_OPTIONS=""
     case $SYSTEM in
-        windows)
-            SYS_OPTIONS="--mingw"
-            ;;
         windows-x86_64)
             SYS_OPTIONS="--mingw --try-64"
             ;;
+        windows*)
+            SYS_OPTIONS="--mingw"
+            ;;
     esac
 
-    rm -rf $OBJ_SYS_DIR
-    local OBJ_DIR="$OBJ_SYS_DIR/0"
-    local PKG_DIR="$OBJ_SYS_DIR/pkg"
     $NDK_DIR/build/tools/build-vendor-host-python.sh --verbose \
         --systems=$SYSTEM $SYS_OPTIONS \
-        --build-dir=$OBJ_DIR \
-        --package-dir=$PKG_DIR $SRC_DIR
+        --build-dir=$OBJ_DIR --package-dir="$DIR_HERE/pkg" \
+        $SRC_DIR
 }
 
 for system in $(echo $SYSTEMS_TO_BUILD | tr ',' ' '); do
