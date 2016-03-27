b93c31c35bc2367c6f649a79fc8225727a34c334
vmurashev <vitaly.murashev@gmail.com>
2016-03-27 14:27:03 +0400

    fix ndk download on darwin

--- ndk-download-latest.sh
+++ ndk-download-latest.sh
@@ -4,7 +4,11 @@ DIR_HERE=$(cd $(dirname $0) && pwd)
 DOWNLOAD_DIR=$DIR_HERE/downloads
 mkdir -p $DOWNLOAD_DIR
 
-CRYSTAX_LATEST_BUILD_URL="https://dl.crystax.net/ndk/linux/latest"
+if [ "$(uname -s)" = "Darwin" ] ; then
+    CRYSTAX_LATEST_BUILD_URL="https://dl.crystax.net/ndk/darwin/latest"
+else
+    CRYSTAX_LATEST_BUILD_URL="https://dl.crystax.net/ndk/linux/latest"
+fi
 CRYSTAX_LATEST_BUILD_FILE="$DOWNLOAD_DIR/crystax.latest-build"
 CRYSTAX_LATEST_BUILD_FILE_TMP=$CRYSTAX_LATEST_BUILD_FILE".tmp"
 rm -f $CRYSTAX_LATEST_BUILD_FILE_TMP
@@ -23,21 +27,30 @@ if [ "$CRYSTAX_LAST_DWNL_BUILD" = "$CRYSTAX_LATEST_BUILD" ]; then
     exit 0
 fi
 
-CRYSTAX_BUILDINFO_URL="https://dl.crystax.net/ndk/linux/$CRYSTAX_LATEST_BUILD/checksum-linux.sha256"
+if [ "$(uname -s)" = "Darwin" ] ; then
+    CRYSTAX_BUILDINFO_URL="https://dl.crystax.net/ndk/darwin/$CRYSTAX_LATEST_BUILD/checksum-darwin.sha256"
+else
+    CRYSTAX_BUILDINFO_URL="https://dl.crystax.net/ndk/linux/$CRYSTAX_LATEST_BUILD/checksum-linux.sha256"
+fi
 CRYSTAX_BUILDINFO_FILE="$DOWNLOAD_DIR/crystax.buildinfo-$CRYSTAX_LATEST_BUILD"
 
 if [ ! -f $CRYSTAX_BUILDINFO_FILE ]; then
-  echo "downloading $CRYSTAX_BUILDINFO_URL ..."
-  curl  -L -o $CRYSTAX_BUILDINFO_FILE $CRYSTAX_BUILDINFO_URL
+    echo "downloading $CRYSTAX_BUILDINFO_URL ..."
+    curl  -L -o $CRYSTAX_BUILDINFO_FILE $CRYSTAX_BUILDINFO_URL
 fi
 
-if [ -n "$(uname -m | grep 64)" ]; then
-  CRYSTAX_DISTR_NAME=$(grep -P '\-x86_64\.' $CRYSTAX_BUILDINFO_FILE | tr '()' '"' | grep -oP '\".*\"' | tr -d '"')
+if [ "$(uname -s)" = "Darwin" ]; then
+    CRYSTAX_DISTR_NAME=$(grep -e -x86_64\\. < $CRYSTAX_BUILDINFO_FILE | tr '()' '"' | grep -o '\".*\"' | tr -d '"')
+    CRYSTAX_URL="https://dl.crystax.net/ndk/darwin/$CRYSTAX_LATEST_BUILD/$CRYSTAX_DISTR_NAME"
 else
-  CRYSTAX_DISTR_NAME=$(grep -P '\-x86\.' $CRYSTAX_BUILDINFO_FILE | tr '()' '"' | grep -oP '\".*\"' | tr -d '"')
+    if [ -n "$(uname -m | grep 64)" ]; then
+        CRYSTAX_DISTR_NAME=$(grep -e -x86_64\\. < $CRYSTAX_BUILDINFO_FILE | tr '()' '"' | grep -o '\".*\"' | tr -d '"')
+    else
+        CRYSTAX_DISTR_NAME=$(grep -e -x86\\. < $CRYSTAX_BUILDINFO_FILE | tr '()' '"' | grep -o '\".*\"' | tr -d '"')
+    fi
+    CRYSTAX_URL="https://dl.crystax.net/ndk/linux/$CRYSTAX_LATEST_BUILD/$CRYSTAX_DISTR_NAME"
 fi
 
-CRYSTAX_URL="https://dl.crystax.net/ndk/linux/$CRYSTAX_LATEST_BUILD/$CRYSTAX_DISTR_NAME"
 CRYSTAX_TARGET_ARC="$DOWNLOAD_DIR/$CRYSTAX_LATEST_BUILD/crystax-ndk.tar.xz"
 echo "crystax url: '$CRYSTAX_URL'"
 echo "download target: '$CRYSTAX_TARGET_ARC'"
