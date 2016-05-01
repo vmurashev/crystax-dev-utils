8023b53388b9f30444f6470279eb24ffb0ed1511
vmurashev <vitaly.murashev@gmail.com>
2016-05-01 20:44:03 +0300

    fix build-cryptography.sh

--- build-cryptography.sh
+++ build-cryptography.sh
@@ -3,8 +3,8 @@ DIR_HERE=$(cd $(dirname $0) && pwd)
 . $DIR_HERE/ndk.pth
 
 ABI_ALL='armeabi,armeabi-v7a,x86,mips,armeabi-v7a-hard,arm64-v8a,x86_64,mips64'
-#ABI_BUILD=$ABI_ALL
-ABI_BUILD=armeabi-v7a
+ABI_BUILD=$ABI_ALL
+#ABI_BUILD=armeabi-v7a
 
 $NDK_DIR/ndk-build -C "$DIR_HERE/python-world/cryptography" APP_ABI=$ABI_BUILD V=1
 $DIR_HERE/mkpyzip.py --src "$DIR_HERE/python-world/cryptography/cryptography" --out "$DIR_HERE/obj/mkpyzip/cryptography.zip"
