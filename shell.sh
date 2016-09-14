c657884a51e66ec7d8b031830694d6275082bd01
vmurashev <vitaly.murashev@gmail.com>
2016-09-14 07:43:58 +0300

    fix stacks corruption on launch c++ tests

--- shell.sh
+++ shell.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
 DIR_HERE=$(cd ${0%shell.sh} && pwd)
 export LD_LIBRARY_PATH=$DIR_HERE/fhs/lib
-export PATH=$DIR_HERE/fhs/bin:$PATH
+export PATH=$PATH:$DIR_HERE/fhs/bin
 cd $DIR_HERE/fhs
 exec /system/bin/sh "$@"
